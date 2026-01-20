a2b68067b5a35ece201dd560a066fdcde5b35f0c
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-01-20 16:47:33 -0300

    aulas

---
+++ curso-em-video/aula11/ex008.js
@@ -0,0 +1,21 @@
+var vel = 12;
+console.log(`A velocidade do seu carro é ${vel}Km/h.`)
+
+// Condições simples:
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. Multa aplicada.')
+}
+console.log('Dirija sempre utilizando um cinto de segurança!')
+
+
+/* 
+TIPOS DE CONDIÇÃO:
+    Tipo 1: Condição Simples
+        if (condição) {
+        }
+
+    Tipo 2: Condição Composta:
+        if (condição) {
+        } else {
+        } 
+*/
---
+++ curso-em-video/aula12/ex011.js
@@ -0,0 +1,24 @@
+var idade = 12
+if (idade < 16) {
+    console.log(`Não Vota`)
+} else if (idade < 18 || idade >= 67) {
+    console.log('Voto opcional')
+} else {
+    console.log(`Voto obrigatório`)
+}
+
+
+
+
+/*
+CONDIÇÕES ANINHADAS:
+        if (condicional 1) {
+            bloco 1
+        } else {
+            if (condicional 2) {
+                bloco 2
+            } else {
+                bloco 3
+            }
+        } 
+*/
