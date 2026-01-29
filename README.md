9ec58d36e6eadd2bdf28f7f4852bdc7d9bd6861c
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-01-29 19:56:50 -0300

    ex015

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
 # JavaScript
 
-Aqui você encontra:
-<p align="center">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png" alt="Logotipo do HTML5" height="50">
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/851px-CSS3_logo_and_wordmark.svg.png" alt="Logotipo do CSS3" height="50">
-    <img src="https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/205/full/javascriptlang.png" alt="Logotipo do JavaScript" height="50">
-</p>
+## Pastas
 
-## Pasta: Aprendizado
+### Curso em Vídeo
+Os itens encontrados nesta pasta servem como práticas elaboradas durante o <a href="https://youtube.com/playlist?list=PLHz_AreHm4dlsK3Nr9GVvXCbpQyHQl1o1&si=TP-oKOSTwTlhjke2">curso de JavaScript</a> do <a href="https://www.youtube.com/@cursoemvideo">Curso em Vídeo</a>.
+
+No atual momento, é a pasta principal do repositório.
+
+### Aprendizado
 Os componentes presentes nesta pasta são baseados no aprendizado baseado na <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript">documentação oficial</a> da <a href="https://developer.mozilla.org/pt-BR/">MDN</a> (Mozilla Developer Network).
 <br><br>
 Todos e quaisquer itens encontrados ali servem para puro aprendizado, não deve ser considerado para outras hipóteses (termo sujeito a alterações).
 
-## Pasta: Testes
-Esta pasta contém testes feitos por mim para alguns resultados. Esta pasta é a mais considerável presente até o momento.
+### Testes
+Esta pasta contém testes feitos por mim para alguns resultados.
 <br><br>
 Veja também o meu curso de HTML5 e CSS3 para entender melhor a minha experiência:
 <br>
--- curso-em-video/aula12ex/ex015/script.js
+++ curso-em-video/aula12ex/ex015/script.js
@@ -1,1 +1,64 @@
-var data = window.document.querySelector('result')
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var aano = document.getElementById('nasc')
+    var res = document.querySelector('div#result')
+
+    if (Number(aano.value) > ano || Number(aano.value) < 1910) {
+        res.innerHTML = `<strong>ERRO!</strong> Verifique o ano e tente novamente.`
+    } else {
+        var sexof = document.getElementsByName('sexo')
+        var idade = ano - Number(aano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sexof[0].checked) {
+            genero = "Homem"
+            if (idade >= 0 && idade <= 10) {
+                // Criança
+                img.setAttribute('src', 'crianca-m.jpg')
+            } else if (idade > 10 && idade <= 25) {
+                // Jovem
+                img.setAttribute('src', 'jovem-m.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-m.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-m.jpg')
+            }
+        }else if (sexof[1].checked) {
+            genero = "Mulher" 
+            if (idade >= 0 && idade <= 10) {
+                // Criança
+                img.setAttribute('src', 'crianca-f.jpg')
+            } else if (idade > 10 && idade <= 25) {
+                // Jovem
+                img.setAttribute('src', 'jovem-f.jpg')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-f.jpg')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-f.jpg')
+            }
+
+            /*
+            if (idade >= 0 && idade <= 10) {
+                //Criança
+                img.setAttribute('src', 'bebê.jpg')
+            } else if (idade < 25) {
+                Jovem
+            } else if (idade < 50) {
+                //Adulto
+            } else {
+                //Idoso
+            }
+            */
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        document.createAttribute = 
+        res.appendChild(img)
+    }
+}
