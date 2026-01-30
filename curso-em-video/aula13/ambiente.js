7e37e0bf449304868d612328b460543e46408402
vmplayer <166970981+vmplayer@users.noreply.github.com>
2026-01-29 21:19:33 -0300

    aula13

---
+++ curso-em-video/aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*
+    ESTRUTURA DE REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+
+let c = 1
+while (c <= 500) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+//----------------------------------------\\
+
+// ESTRUTURA COM TESTE LÓGICO NO FINAL
+
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
